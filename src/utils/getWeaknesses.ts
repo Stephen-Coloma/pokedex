bdbe8c4ce2a70915c339359e95e4dff790dbacd7
Stephen Coloma <2232847@slu.edu.ph>
2025-03-31 22:17:42 +0800

    util(function getWeaknesses) a function that retrieves pokemon's weaknesses
    Implemented the logic using set and arrays in retrieving pokemon's types weaknesses
    The source of these weaknesses comes from this link: https://www.eurogamer.net/pokemon-go-type-chart-effectiveness-weaknesses

---
+++ src/utils/getWeaknesses.ts
@@ -0,0 +1,47 @@
+import { PokemonType } from "@/types/Pokemon";
+
+/**
+ * This represents the record of the weaknesses of each type of pokemon
+ * The reference for this record is based on this link: https://www.eurogamer.net/pokemon-go-type-chart-effectiveness-weaknesses
+ */
+const typeWeaknesses: Record<PokemonType, PokemonType[]> = {
+  Normal: ["Fighting"],
+  Fire: ["Ground", "Rock", "Water"],
+  Water: ["Grass", "Electric"],
+  Electric: ["Ground"],
+  Grass: ["Flying", "Poison", "Bug", "Fire", "Ice"],
+  Ice: ["Fighting", "Rock", "Steel", "Fire"],
+  Fighting: ["Flying", "Psychic", "Fairy"],
+  Poison: ["Ground", "Psychic"],
+  Ground: ["Water", "Grass", "Ice"],
+  Flying: ["Rock", "Electric", "Ice"],
+  Psychic: ["Bug", "Ghost", "Dark", "Psychic"],
+  Bug: ["Flying", "Rock", "Fire"],
+  Rock: ["Fighting", "Ground", "Steel", "Water", "Grass"],
+  Ghost: ["Ghost", "Dark"],
+  Dragon: ["Ice", "Dragon", "Fairy"],
+  Dark: ["Fighting", "Bug", "Fairy"],
+  Steel: ["Fighting", "Ground", "Fire"],
+  Fairy: ["Poison", "Steel"],
+};
+
+/**
+ * This function returns the weaknesses of the pokemon type/s.
+ * It creates a set that will contain the final returned value.
+ * It iterates the given type/s of the pokemon, and using the current type, 
+ * it retrieves the weaknesses of that current type from the record.
+ * 
+ * @param types the pokemon type/s 
+ * @returns the types where the pokemon is vulnerable to.
+ */
+function getWeaknesses(types: PokemonType[]): PokemonType[] {
+  const weaknesses = new Set<PokemonType>();
+
+  types.forEach((currentType) => {
+    typeWeaknesses[currentType]?.forEach((weaknessesOfCurrentType) => {
+      weaknesses.add(weaknessesOfCurrentType);
+    });
+  });
+
+  return Array.from(weaknesses);
+}

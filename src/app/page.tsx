e1e72c6b870f4789b0b968428ec71596fa216510
Stephen Coloma <2232847@slu.edu.ph>
2025-04-02 13:00:00 +0800

    feat(sorting) sorting functionality
    This commits adds the functionality of sorting based on id and name for both ascending and descending.
    
    Todo: implement a lazy loading for the data when we are sorting it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,34 @@ import { SortDropDownMenu } from "@/components/sort-dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { useFetchAllPokemons } from "@/hooks/useFetchPokemons";
 import { useState } from "react";
-``
+``;
 export default function Home() {
   const { status, statusText, data, error, loading } = useFetchAllPokemons();
   const [cardPerPage, setCardPerPage] = useState<number>(10);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  
+  const [sortOption, setSortOption] = useState<string>("id-ascending");
+
   const totalPages = data ? Math.ceil(data.length / cardPerPage) : 0;
 
+  //appy sorting to the data when it is available
+  // if negative value, a comes before b, otherwise a comes after b 
+  const sortedData = data
+    ? [...data].sort((pokemon1, pokemon2) => {
+        if (sortOption === "name-ascending")
+          return pokemon1.name.localeCompare(pokemon2.name);
+        if (sortOption === "name-descending")
+          return pokemon2.name.localeCompare(pokemon1.name);
+        if (sortOption === "id-ascending") return pokemon1.id - pokemon2.id;
+        if (sortOption === "id-descending") return pokemon2.id - pokemon1.id;
+        return 0;
+      })
+    : [];
+
+  //pagination
   const indexOfLastCard = currentPage * cardPerPage; //index 1 * 10 = 10
   const indexOfFirstCard = indexOfLastCard - cardPerPage; // 10 - 10 = 0;
-  const visibleCards = data
-    ? data.slice(indexOfFirstCard, indexOfLastCard)
+  const visibleCards = sortedData
+    ? sortedData.slice(indexOfFirstCard, indexOfLastCard)
     : [];
 
   const handlePreviousPage = () => {
@@ -29,16 +45,19 @@ export default function Home() {
     setCurrentPage((prev) => (prev < totalPages ? prev + 1 : prev));
   };
 
+  const handleSortChange = (sortOptionChosen: string) => {
+    setSortOption(sortOptionChosen);
+  };
+
   return (
     <>
-      <ModeToggle/>
-      <SortDropDownMenu></SortDropDownMenu>
+      <ModeToggle />
+      <SortDropDownMenu onSortChange={handleSortChange}></SortDropDownMenu>
       <div className="mx-auto w-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
         {loading &&
-          new Array(8).fill('t').map((_, index) => (
-            <PokemonCardSkeleton key={index}/>
-          ))
-        }
+          new Array(8)
+            .fill("t")
+            .map((_, index) => <PokemonCardSkeleton key={index} />)}
 
         {visibleCards &&
           visibleCards.map((pokemon, index) => (
@@ -48,7 +67,7 @@ export default function Home() {
             </div>
           ))}
       </div>
-      
+
       <div className="flex justify-center gap-4 w-full my-10">
         <Button onClick={handlePreviousPage}>Prev</Button>
         <Button onClick={handleNextPage}>Next</Button>
--- src/components/sort-dropdown-menu.tsx
+++ src/components/sort-dropdown-menu.tsx
@@ -14,27 +14,44 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ArrowDownWideNarrow } from "lucide-react";
 
-export function SortDropDownMenu() {
-  const [position, setPosition] = React.useState("id-ascending");
+export type SortDropDownMenuProps = {
+  onSortChange: (sortOption: string) => void;
+}
+
+export function SortDropDownMenu({onSortChange}: SortDropDownMenuProps) {
+  const [sortOption, setSortOption] = React.useState("id-ascending");
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline">
-          <ArrowDownWideNarrow/>
+          <ArrowDownWideNarrow/>  
           Sort
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
+      <DropdownMenuContent className="w-fit">
         <DropdownMenuLabel>Filter by Name</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+        <DropdownMenuSeparator /> 
+        <DropdownMenuRadioGroup 
+          value={sortOption}
+          onValueChange={(value) => {
+            setSortOption(value);
+            onSortChange(value); // Notify parent component
+          }}
+        >
           <DropdownMenuRadioItem value="name-ascending">Aa - Zz</DropdownMenuRadioItem>
           <DropdownMenuRadioItem value="name-descending">Zz - Aa</DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
+
         <DropdownMenuLabel>Filter by ID</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+        <DropdownMenuRadioGroup
+          value={sortOption}
+          onValueChange={(value) => {
+            setSortOption(value);
+            onSortChange(value); // Notify parent component
+          }}
+        >
           <DropdownMenuRadioItem value="id-ascending">1 - 1025</DropdownMenuRadioItem>
           <DropdownMenuRadioItem value="id-descending">1025 - 1</DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>

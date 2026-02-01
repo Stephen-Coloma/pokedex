a208a1a087fd6ea731a780a58d66c7d272801d2a
Stephen Coloma <2232847@slu.edu.ph>
2026-02-02 01:09:51 +0800

    fix: image configuration on nextConfig as old code was deprecated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['assets.pokemon.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.pokemon.com",
+      },
+    ],
   },
 };
 
